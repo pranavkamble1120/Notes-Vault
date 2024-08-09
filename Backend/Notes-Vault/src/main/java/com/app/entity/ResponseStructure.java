b31bdd6b25e73d233df3258eb7b79e6c908c2aaa
Pranavkamble1120 <vpk2489@gmail.com>
2024-08-09 11:08:32 +0530

    commit

---
+++ Backend/Notes-Vault/src/main/java/com/app/entity/ResponseStructure.java
@@ -0,0 +1,29 @@
+package com.app.entity;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Setter
+public class ResponseStructure<T> {
+
+	private T Data;
+	private String message;
+	private int statusCode;
+	
+	public void setData(T string) {
+		// TODO Auto-generated method stub
+		Data = string;
+	}
+	
+	public void setMessage(String message2) {
+		// TODO Auto-generated method stub
+		message = message2;
+	}
+	
+	public void setStatusCode(int value) {
+		// TODO Auto-generated method stub
+		statusCode = value;
+	}
+	
+}
